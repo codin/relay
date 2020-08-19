dbbf527c7780f11490365ecf6cedf934b9f54b26
Kieron <kieron@fastmail.fm>
2020-08-19 09:54:56 +0100

    make it so!

---
+++ spec/Codin/RelaySpec.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace spec\Codin;
+
+use PhpSpec\ObjectBehavior;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+
+class RelaySpec extends ObjectBehavior
+{
+    public function it_should_process_middleware(ServerRequestInterface $request, ResponseInterface $response, MiddlewareInterface $first, MiddlewareInterface $second)
+    {
+        $this->beConstructedWith([$first, $second]);
+        $first->process($request, $this)->willReturn($response);
+        $second->process($request, $this)->willReturn($response);
+        $this->handle($request)->shouldReturn($response);
+    }
+}
---
+++ src/Relay.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codin;
+
+use Closure;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class Relay implements RequestHandlerInterface
+{
+    /**
+     * @var array<MiddlewareInterface>
+     */
+    protected $queue;
+
+    /**
+     * @var Closure
+     */
+    protected $resolver;
+
+    public function __construct(array $queue, ?callable $resolver = null)
+    {
+        if (count($queue) === 0) {
+            throw new RelayException('queue cannot be empty');
+        }
+        $this->queue = $queue;
+        reset($this->queue);
+        $this->resolver = is_callable($resolver) ?
+            Closure::fromCallable($resolver) :
+            static function (MiddlewareInterface $entry): MiddlewareInterface {
+                return $entry;
+            };
+    }
+
+    public function nextMiddleware(): MiddlewareInterface
+    {
+        $entry = current($this->queue);
+        $middleware = ($this->resolver)($entry);
+        next($this->queue);
+        return $middleware;
+    }
+
+    public function handle(ServerRequestInterface $request) : ResponseInterface
+    {
+        return $this->nextMiddleware()->process($request, $this);
+    }
+}
