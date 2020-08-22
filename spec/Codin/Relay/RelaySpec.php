00c064bb1b8f1abc6a125cbce3a6631a25db0919
Kieron <kieron@fastmail.fm>
2020-08-22 15:13:25 +0100

    tests

--- spec/Codin/Relay/RelaySpec.php
+++ spec/Codin/Relay/RelaySpec.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace spec\Codin\Relay;
 
 use PhpSpec\ObjectBehavior;
-
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
